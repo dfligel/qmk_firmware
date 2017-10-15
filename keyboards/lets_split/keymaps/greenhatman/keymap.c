e0d1f179595079ae6d2e8695f78ef318b8b0697f
DS Fligel <dfligel@gmail.com>
2017-10-15 01:50:31 +0100

    prototype LS map

--- keyboards/lets_split/keymaps/greenhatman/keymap.c
+++ keyboards/lets_split/keymaps/greenhatman/keymap.c
@@ -89,18 +89,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  TAB |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   `  |   '  |   #  |   /  |   {  |   }  |   _  |   +  |   |  |   ;  | Esc  |
+ * |      |   `  |   '  |   #  |   /  |   {  |   }  |   _  |   +  |   |  |   ;  | Esc  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|  F5  |  F6  |  F7  |  F8  |   <  |   >  |      |      |      |      | Shift|
+ * |      |  F5  |  F6  |  F7  |  F8  |   <  |   >  |      |      | Next | Voi+ | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      | Play | Prev | Vol- | Mute |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = KEYMAP( \
-  KC_TAB, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-  _______, KC_GRV,   KC_F2,   KC_HASH,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
+  _______, KC_GRV, KC_QUOT, KC_HASH, KC_SLSH, KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS, KC_PIPE, KC_SCLN, KC_ESC, \
+  _______, KC_F5, KC_F6, KC_F7, KC_F8, S(KC_COMMA), S(KC_DOT), _______, _______, KC_MNXT, KC_VOLU, KC_RSFT, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MPRV, KC_VOLD, KC_MUTE \
 ),
 
 /* Raise
@@ -115,10 +115,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = KEYMAP( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+  _______, KC_TILD, S(KC_QUOT), S(KC_SLSH), KC_BSLS, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL, _______, KC_COLN, _______, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_LBRC,  KC_RBRC,  KC_F9, KC_F10, KC_F11, KC_F12, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
 /* Adjust (Lower + Raise)
@@ -154,7 +154,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LOWER, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_ESC,  TO(_QWERTY), KC_LALT, KC_LALT, KC_LALT,   KC_SPC,  KC_ENT,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  LOWER,  TO(_QWERTY), KC_LALT, KC_LALT, KC_LALT,   KC_SPC,  KC_ENT,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 )
 
 
