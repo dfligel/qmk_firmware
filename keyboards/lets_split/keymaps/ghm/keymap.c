c55f0b0bdbbda7b44ce34571caebcd08ade83d76
DS Fligel <dfligel@gmail.com>
2017-11-08 17:21:00 +0000

    Finalised Let's split and baseline planck config

--- keyboards/lets_split/keymaps/ghm/keymap.c
+++ keyboards/lets_split/keymaps/ghm/keymap.c
@@ -35,18 +35,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Esc  |
+ * | CTL-E|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |CTL-E |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | GAME | GUI  | ALT  |Lower |Space |Enter |Raise | Left | Down |  Up  |Right |
+ * | MEH  |ADJUST| GUI  | ALT  |Lower |Space |Enter |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ESC, \
+  CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_T(KC_ESC), \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  MEH(KC_ESC),  TO(_GAME), KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_MEH, ADJUST, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Colemak
@@ -97,8 +97,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = KEYMAP( \
-  KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-  _______, KC_GRV, KC_QUOT, KC_HASH, KC_SLSH, KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS, KC_PIPE, KC_SCLN, KC_ESC, \
+  _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
+  _______, KC_GRV, KC_QUOT, KC_HASH, KC_SLSH, KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS, KC_PIPE, KC_SCLN, _______, \
   _______, KC_F5, KC_F6, KC_F7, KC_F8, S(KC_COMMA), S(KC_DOT), _______, _______, KC_MPLY, KC_MUTE, KC_RSFT, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT \
 ),
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = KEYMAP( \
-  KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+  _______,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
   _______, KC_TILD, S(KC_QUOT), S(KC_SLSH), KC_BSLS, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL, KC_PIPE, KC_COLN, _______, \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_LBRC,  KC_RBRC,  KC_F9, KC_F10, KC_F11, KC_F12, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______ \
